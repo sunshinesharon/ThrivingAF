ffd29f7e8f1833b0331829b37498c67ce861152e
Sharon Jacob <fab8925@gmail.com>
2024-06-11 15:22:56 -0400

    updated pricing page just for aesthetic

--- client/src/pages/pricing/Pricing.jsx
+++ client/src/pages/pricing/Pricing.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import HomeGif from "../../assets/images/taf.gif";
+import BetaBanner from "../../assets/images/Beta Banner.gif"
+import { useNavigate } from 'react-router-dom';
 import '../pricing/Pricing.scss';
 
 const Pricing = () => {
   const [showForm, setShowForm] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -13,7 +17,7 @@ const Pricing = () => {
         setShowForm(false);
       }
     };
-
+  
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -23,25 +27,28 @@ const Pricing = () => {
   return (
     <div className="pricing">
       <div className="pricing__banner">
-        <img src="taf.gif" alt="Beta Program Banner" className="pricing__banner-image" />
+        <img src={HomeGif} alt="Beta Program Banner" className="pricing__banner-image" />
       </div>
       <div className={`pricing__form-container ${showForm ? 'pricing__form-container--visible' : ''}`}>
         <form className="pricing__form">
-          <h2 className="pricing__form-title">Luuna's Beta Program</h2>
+          <h2 className="pricing__form-title">Becoming a ThrivingAF Insider</h2>
           <div className="pricing__form-group">
-            <label htmlFor="name" className="pricing__form-label">Name</label>
-            <input type="text" id="name" className="pricing__form-input" />
+            <label htmlFor="name" className="pricing__form-label">Your Name</label>
+            <input type="name" id="name" className="pricing__form-input" />
           </div>
           <div className="pricing__form-group">
-            <label htmlFor="email" className="pricing__form-label">Email</label>
+            <label htmlFor="email" className="pricing__form-label">Early Access Granted to which Email?</label>
             <input type="email" id="email" className="pricing__form-input" />
           </div>
           <div className="pricing__form-group">
             <label htmlFor="message" className="pricing__form-label">Tell us why you'd be an awesome beta tester</label>
             <textarea id="message" className="pricing__form-textarea"></textarea>
           </div>
-          <button type="submit" className="pricing__form-button">Apply to join beta program</button>
+          <button type="submit" className="pricing__form-button" onClick={() => navigate('/')}>Activate Thrive Mode</button>
         </form>
+      </div>
+      <div className="pricing__info-section">
+        <img src={BetaBanner} alt="Info Section Banner" className="pricing__info-image" />
       </div>
     </div>
   );
